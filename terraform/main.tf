7259a7d141187f84dd4335636433a99ee08a4b44
Ankur Tripathi <ankur033.tripathi@gmail.com>
2025-04-16 01:52:18 +0530

    tf syntax error

--- terraform/main.tf
+++ terraform/main.tf
@@ -108,7 +108,7 @@ resource "aws_lambda_function" "app" {
   function_name = var.project_name
   role          = aws_iam_role.lambda_role.arn
   package_type  = "Image"
-  image_uri     = "${data.aws_ecr_repository.app.repository_url}:${var.image_tag}""
+  image_uri     = "${data.aws_ecr_repository.app.repository_url}:${var.image_tag}"
   timeout       = 60
   memory_size   = 1024
 
@@ -128,7 +128,6 @@ resource "aws_lambda_function" "app" {
     Project     = var.project_name
   }
 }
-
 # API Gateway REST API
 resource "aws_apigatewayv2_api" "api" {
   name          = "${var.project_name}-api"
