2100da691bc5201ed233395d30917b9bc5d5a4d6
Ankur Tripathi <ankur033.tripathi@gmail.com>
2025-04-16 01:20:51 +0530

    use lambda compatible base image

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from datetime import datetime
 import uvicorn
+from mangum import Mangum
 
 app = FastAPI(title="SimpleTimeService")
 
@@ -11,6 +12,7 @@ async def get_time_and_ip(request: Request):
         "timestamp": datetime.now().isoformat(),
         "ip": client_ip
     }
+handler = Mangum(app)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
